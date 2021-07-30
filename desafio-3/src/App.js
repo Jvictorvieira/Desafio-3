f53adb11bb315f8e157f0258767385cdc9934520
Jvictorvieira <joaoovictor.21@gmail.com>
2021-07-30 01:49:03 -0300

    refactor: correção nos Botões

--- desafio-3/src/App.js
+++ desafio-3/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <div className='button-layout' >
             <nav >
               <button type='button' value='Home' name="Home" onClick='' className='button'>Home</button>
-              <button type='button' value='Deliver' name="Seed Delivery" onClick='{Changevalue(value)}' className='button'>Seed Delivery</button>
+              <button type='button' value='App' name="Seed App" onClick='{Changevalue(value)}' className='button'>Seed Delivery</button>
               <button type='button' value='Site' name="Site Constitucional" onClick='{Changevalue(value)}' className='button'>Site Constitucional</button>
               <button type='button' value='Consul' name="Consultoria" onClick='{Changevalue(value)}' className='button'>Consultoria</button>
               <button type='button' value='Cont' name="Contatos" onClick='{Changevalue(value)}' className='button'>Contatos</button>
--- desafio-3/src/Body.js
+++ desafio-3/src/Body.js
@@ -6,29 +6,41 @@ function Body (value) {
     if (value ==='Home') {
         return (
             <div className='Container'>
-                <h2 className='subtitle'>About us</h2>
-                <p></p>
+                <div>
+                    <h2 className='subtitle'>About us</h2>
+                    <p></p>
+                </div>
+                <img></img>
             </div>
         )
     } else if (value ==='Site') {
         return (
             <div className='Container'>
-                <h2 className='subtitle'>Site Constitucional</h2>
-                <p></p>
+                <div>
+                    <h2 className='subtitle'>Site Constitucional</h2>
+                    <p></p>
+                </div>
+                <img></img>
             </div>
         )   
     } else if (value ==='Consul') {
         return (
             <div className='Container'>
-                <h2 className='subtitle'>Consultoria</h2>
-                <p></p>
+                <div>
+                    <h2 className='subtitle'>Consultoria</h2>
+                    <p></p>
+                </div>
+                <img></img>
             </div>
         )   
-    } else if (value==='Deliver') {
+    } else if (value==='App') {
         return (
             <div className='Container'>
-                <h2 className='subtitle'>Seed Delivery</h2>
-                <p></p>
+                <div>
+                    <h2 className='subtitle'>Seed app</h2>
+                    <p></p>
+                </div>
+                <img></img>
             </div>
         )   
     }  

5b4c67e50f35ec8eb591e3b0645653c1fd3444cd
Jvictorvieira <joaoovictor.21@gmail.com>
2021-07-29 23:38:42 -0300

    Feat: titulo e criação dos botões

--- desafio-3/src/App.js
+++ desafio-3/src/App.js
@@ -2,12 +2,37 @@ import React from "react";
 import Body from "./Body.js";
 
 
-
+var value = 'Home'
 
 
 
 function App() {
-  
+  function Changevalue (valor) {
+    return value=valor
+  }
+  return (
+    <div>
+        <header>
+          <div className='nav-bar title'>
+          <h2>Seed a Bit</h2>
+          <nav>
+            <button type='button' value='Home' name="Home" onClick='' className='button'>Home</button>
+            <button type='button' value='Deliver' name="Seed Delivery" onClick='{Changevalue(value)}' className='button'>Seed Delivery</button>
+            <button type='button' value='Site' name="Site Constitucional" onClick='{Changevalue(value)}' className='button'>Site Constitucional</button>
+            <button type='button' value='Consul' name="Consultoria" onClick='{Changevalue(value)}' className='button'>Consultoria</button>
+            <button type='button' value='Cont' name="Contatos" onClick='{Changevalue(value)}' className='button'>Contatos</button>
+          </nav>
+          </div>
+        </header>
+        <div className='body'>
+          <div>{
+            Body(value)
+            }</div>
+
+        </div>
+      
+    </div>
+  );
 }
 
 export default App;
--- desafio-3/src/Body.js
+++ desafio-3/src/Body.js
@@ -1,3 +1,38 @@
 import React from "react";
 
 
+function Body (value) {
+    if (value ==='Home') {
+        return (
+            <div>
+                <p>oioioiooii</p>
+            </div>
+        )
+    } else if (value ==='Site') {
+        return (
+            <div>
+                <p>Abrahom</p>
+            </div>
+        )   
+    } else if (value ==='Consul') {
+        return (
+            <div>
+                <p>Pirocudo</p>
+            </div>
+        )   
+        } else if (value==='Deliver') {
+        return (
+            <div>
+                <p>bucetudo</p>
+            </div>
+        )   
+    } else if (value ==='Cont') {
+        return (
+            <div>
+                <p>aaasasaaaa</p>
+            </div>
+        )   
+    }
+}
+
+export default Body;
